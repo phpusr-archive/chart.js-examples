a85209cd0405d0c7eaf29075c28ddbffe764612c
phpusr <phpusr@gmail.com>
2023-11-22 16:16:47 +0500

    Отключил анимацию, легенду и всплывающие подсказки

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ const data = [
 
 const config =  {
   type: 'bar',
+  options: {
+    animation: false,
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        enabled: false
+      }
+    }
+  },
   data: {
     labels: data.map(row => row.year),
     datasets: [
