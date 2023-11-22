7099730cf977fc108cf8a22dd3e68eb27d316c79
phpusr <phpusr@gmail.com>
2023-11-22 20:32:45 +0500

    Добавил прокрутку графика с асинхронной загрузкой данных

--- main.js
+++ main.js
@@ -1,33 +1,72 @@
 
 const data = [
-  { year: 2010, count: 10 },
-  { year: 2011, count: 20 },
-  { year: 2012, count: 15 },
-  { year: 2013, count: 25 },
-  { year: 2014, count: 22 },
-  { year: 2015, count: 30 },
-  { year: 2016, count: 28 },
+  { time: 1, min: 0, max: 0 },
+  { time: 2, min: 10, max: 12 },
+  { time: 3, min: 15, max: 17 }
 ]
 
+const size = 2000
+const minValue = 0
+const maxValue = 1000
+let prevValue = 20
+for (let i = 4; i < size; i++) {
+  const up = Math.random() > 0.5
+  const diff = Math.random() * 10
+  const min = prevValue + (diff * up ? 1 : -1)
+  const max = min + Math.random() * 2
+  data.push({
+    time: i,
+    min,
+    max
+  })
+  prevValue = min
+}
+
 const config =  {
-  type: 'bar',
+  type: 'line',
   options: {
     animation: false,
-    plugins: {
-      legend: {
-        display: false
+    indexAxis: 'y',
+    elements: {
+      point: {
+        radius: 0
       },
-      tooltip: {
-        enabled: false
+      line: {
+        borderWidth: 2
+      }
+    },
+    scales: {
+      x: {
+        min: 0,
+        max: 100,
       }
     }
   },
   data: {
-    labels: data.map(row => row.year),
+    labels: [],
     datasets: [
       {
-        label: 'Acquisitions by year',
-        data: data.map(row => row.count)
+        label: 'Test (min)',
+        backgroundColor: "#693a9d",
+        borderColor: "#693a9d",
+        data: [],
+        parsing: {
+          xAxisKey: "min",
+          yAxisKey: "time"
+        },
+        fill: 1
+      },
+      {
+        label: 'Test (max)',
+        backgroundColor: "#693a9d",
+        borderColor: "#693a9d",
+        data: [],
+        parsing: {
+          xAxisKey: "max",
+          yAxisKey: "time"
+        },
+        fill: false,
+        hidden: false
       }
     ]
   }
@@ -36,4 +75,48 @@ const config =  {
 const myChart = new Chart(
   document.getElementById('myChart'),
   config
-);
+)
+
+const chunkSize = 100
+let offset = 0
+let diffOffset = 0
+let loadTimeout
+
+updateData()
+
+
+function updateData() {
+  if (loadTimeout) {
+    clearTimeout(loadTimeout)
+  }
+
+  offset += diffOffset
+  const axisData = data.slice(offset, offset + chunkSize)
+  myChart.data.labels = axisData.map(it => it.time)
+  myChart.data.datasets.forEach(dataset => {
+    const fromIndex = diffOffset > 0 ? diffOffset : 0
+    const toIndex = diffOffset > 0 ? dataset.data.length : dataset.data.length + diffOffset
+    console.log({ fromIndex, toIndex })
+    dataset.data = dataset.data.slice(fromIndex, toIndex)
+  })
+  myChart.update()
+
+  loadTimeout = setTimeout(() => {
+    myChart.data.datasets.forEach(dataset => {
+      dataset.data = axisData.slice()
+    })
+    myChart.update()
+  }, 500)
+}
+
+document.querySelector('#scrollDown').addEventListener('click', () => {
+  diffOffset = 10
+  console.log({ offset })
+  updateData()
+})
+
+document.querySelector('#scrollUp').addEventListener('click', () => {
+  diffOffset = -10
+  console.log({ offset })
+  updateData()
+})
