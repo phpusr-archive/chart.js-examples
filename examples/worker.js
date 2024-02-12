f73c759e17320a060b519e641778f2a5c6db3121
phpusr <phpusr@gmail.com>
2024-02-12 22:29:22 +0500

    Доработка worker

--- examples/worker.js
+++ examples/worker.js
@@ -1,12 +1,26 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.js')
 
+const data = {}
 
 onmessage = function (event) {
-  const {canvas, config} = event.data;
-  const chart = new Chart(canvas, config);
+  //console.log('this', this)
 
-  // Resizing the chart must be done manually, since OffscreenCanvas does not include event listeners.
-  //canvas.width = 600;
-  //canvas.height = 800;
-  chart.resize();
+  const { action ,canvas, config } = event.data
+  console.log('canvas', canvas, event.data.config)
+
+  if (action === 'create') {
+    data.chart = new Chart(canvas, config);
+  } else if (action === 'update') {
+    data.chart.data.datasets.forEach(dataset => {
+      dataset.data.push({ x: 12, y: 12 })
+    })
+    console.log('dataset', data.chart.data.datasets)
+    data.chart.update()
+
+    // Resizing the chart must be done manually, since OffscreenCanvas does not include event listeners.
+    // canvas.width = 600;
+    // canvas.height = 800;
+    // chart.resize();
+  }
+
 };
